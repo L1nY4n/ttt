861d3b0e068868fd172e936107151efde045912f
l-y <l1ny4n@gmail.com>
2024-12-08 11:19:57 +0800

    fix multi-select warning

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,8 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+
+
+
+const config =  {
   darkMode: ["class"],
   content: [
     './pages/**/*.{ts,tsx}',
@@ -73,5 +76,7 @@ module.exports = {
       },
     },
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [],
 }
+
+export default config;
